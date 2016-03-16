865e84597af6cd2ce30b421260d943f4b7c3ac9f
Jasper Van Damme <jasper.van.damme@student.howest.be>
2016-03-16 19:01:45 +0100

    Added webpack and other dependencies

---
+++ dao/DAO.php
@@ -0,0 +1,18 @@
+<?php
+class DAO {
+	private static $dbHost = 'localhost';
+	private static $dbName = 'bw_bookworm';
+	private static $dbUser = 'bw_user';
+	private static $dbPass = '$bw_pass';
+	private static $sharedPDO;
+	protected $pdo;
+	function __construct() {
+		if(empty(self::$sharedPDO)) {
+			self::$sharedPDO = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName, self::$dbUser, self::$dbPass);
+			self::$sharedPDO->exec("SET CHARACTER SET utf8");
+			self::$sharedPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			self::$sharedPDO->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		}
+		$this->pdo =& self::$sharedPDO;
+	}
+}
