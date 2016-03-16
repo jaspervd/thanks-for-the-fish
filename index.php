a102132433db02506b8807a1960b11ff88fc8fe5
Jasper Van Damme <jasper.van.damme@student.howest.be>
2016-03-16 19:37:55 +0100

    Added /api/teachers to return all teachers

--- index.php
+++ index.php
@@ -6,13 +6,21 @@ define('DS', DIRECTORY_SEPARATOR);
 define('WWW_ROOT', __DIR__ . DS);
 
 require 'vendor/autoload.php';
+require 'dao/TeachersDAO.php';
 
 $app = new \Slim\App;
+$teachersDAO = new TeachersDAO();
 
-$app->get('/{anything:.*}', function ($request, $response, $args) {
+$app->get('/{anything:.*}', function($request, $response, $args) {
 	$view = new \Slim\Views\PhpRenderer('view/');
 	$basePath = $request->getUri()->getBasePath();
 	return $view->render($response, 'home.php', ['basePath' => $basePath]);
+});
+
+$app->get('/api/teachers/?', function($teachersDAO) {
+    header('Content-Type: application/json');
+    echo json_encode($teachersDAO->getTeachers());
+    exit();
 });
 
 $app->run();
