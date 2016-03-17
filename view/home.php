c6a0223e1328af0d8a838112803f1775d92fed9b
Jasper Van Damme <jasper.van.damme@student.howest.be>
2016-03-17 17:41:58 +0100

    Added the order form to home.php

--- view/home.php
+++ view/home.php
@@ -16,6 +16,51 @@
 		<div class="countdown-minutes">0</div>
 		<div class="countdown-seconds">0</div>
 	</div>
+	<form method="post" action="">
+		<p>
+			<label for="firstname">Voornaam</label>
+			<input type="text" name="firstname" id="firstname" required />
+		</p>
+		<p>
+			<label for="lastname">Achternaam</label>
+			<input type="text" name="lastname" id="lastname" required />
+		</p>
+		<p>
+			<label for="email">E-mailadres</label>
+			<input type="email" name="email" id="email" required />
+		</p>
+		<p>
+			<label for="number">Telefoonnummer</label>
+			<input type="tel" name="number" id="number" placeholder="0412 34 45 67" required />
+		</p>
+		<p>
+			<label for="password">Wachtwoord</label>
+			<input type="password" name="password" id="password" required />
+		</p>
+		<p>
+			<label for="password-repeat">Wachtwoord herhalen</label>
+			<input type="password" name="password-repeat" id="password-repeat" required />
+		</p>
+		<p>
+			<label for="school-name">Schoolnaam</label>
+			<input type="text" name="school-name" id="school-name" required />
+		</p>
+		<p>
+			<label for="school-email">School e-mailadres</label>
+			<input type="text" name="school-email" id="school-email" required />
+		</p>
+		<p>
+			<label for="school-address">Schooladres</label>
+			<input type="text" name="school-address" id="school-address" required />
+		</p>
+		<p>
+			<label for="school-website">Schoolwebsite</label>
+			<input type="url" name="school-website" id="school-website" required />
+		</p>
+		<p>
+			<input type="submit" name="submit" value="Bestellen" />
+		</p>
+	</form>
 	<script src="<?php echo $basePath; ?>/js/script.js"></script>
 </body>
 </html>
