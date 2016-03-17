3f347157b64594798264d57f2f1481a82a89defd
Jasper Van Damme <jasper.van.damme@student.howest.be>
2016-03-18 00:24:04 +0100

    Form data saves to database

--- _js/script.js
+++ _js/script.js
@@ -2,16 +2,41 @@
 
 import Countdown from './classes/Countdown';
 
-const init = () => {
-	let countdown = new Countdown(new Date(2016, 4, 18, 20, 0, 0)); // 18 mei 2016 om 20u00
-	countdown.start();
+(() => {
+	let orderForm = document.getElementsByClassName('order-form')[0];
 
-	countdown.on('tick', () => {
-		document.getElementsByClassName('countdown-days')[0].innerHTML = countdown.days;
-		document.getElementsByClassName('countdown-hours')[0].innerHTML = countdown.hours;
-		document.getElementsByClassName('countdown-minutes')[0].innerHTML = countdown.minutes;
-		document.getElementsByClassName('countdown-seconds')[0].innerHTML = countdown.seconds;
-	});
-};
+	const init = () => {
+		let countdown = new Countdown(new Date(2016, 4, 18, 20, 0, 0)); // 18 mei 2016 om 20u00
+		countdown.start();
 
-init();
+		countdown.on('tick', () => {
+			document.getElementsByClassName('countdown-days')[0].innerHTML = countdown.days;
+			document.getElementsByClassName('countdown-hours')[0].innerHTML = countdown.hours;
+			document.getElementsByClassName('countdown-minutes')[0].innerHTML = countdown.minutes;
+			document.getElementsByClassName('countdown-seconds')[0].innerHTML = countdown.seconds;
+		});
+
+		orderForm.addEventListener('submit', orderHandler);
+	};
+
+	const orderHandler = (e) => {
+		e.preventDefault();
+
+		let formData = new FormData(orderForm);
+
+		let request = new XMLHttpRequest();
+		request.open('POST', `${window.app.basename}/api/teachers`, true);
+		request.onload = function() {
+			if (request.status === 201) {
+				console.log('Besteld!');
+			} else {
+				console.log('Fout');
+			}
+		};
+
+		request.send(formData);
+		return true;
+	};
+
+	init();
+})();
--- view/home.php
+++ view/home.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<title>Klassiekers in je klas</title>
 	<link rel="stylesheet" href="<?php echo $basePath; ?>/css/style.css">
 </head>
@@ -16,7 +17,7 @@
 		<div class="countdown-minutes">0</div>
 		<div class="countdown-seconds">0</div>
 	</div>
-	<form method="post" action="">
+	<form method="post" action="" class="order-form">
 		<p>
 			<label for="firstname">Voornaam</label>
 			<input type="text" name="firstname" id="firstname" required />
@@ -30,37 +31,41 @@
 			<input type="email" name="email" id="email" required />
 		</p>
 		<p>
-			<label for="number">Telefoonnummer</label>
-			<input type="tel" name="number" id="number" placeholder="0412 34 45 67" required />
+			<label for="phone">Telefoonnummer</label>
+			<input type="tel" name="phone" id="phone" placeholder="0412 34 45 67" required />
 		</p>
 		<p>
 			<label for="password">Wachtwoord</label>
 			<input type="password" name="password" id="password" required />
 		</p>
 		<p>
-			<label for="password-repeat">Wachtwoord herhalen</label>
-			<input type="password" name="password-repeat" id="password-repeat" required />
+			<label for="password_repeat">Wachtwoord herhalen</label>
+			<input type="password" name="password_repeat" id="password_repeat" required />
 		</p>
 		<p>
-			<label for="school-name">Schoolnaam</label>
-			<input type="text" name="school-name" id="school-name" required />
+			<label for="school_name">Schoolnaam</label>
+			<input type="text" name="school_name" id="school_name" required />
 		</p>
 		<p>
-			<label for="school-email">School e-mailadres</label>
-			<input type="text" name="school-email" id="school-email" required />
+			<label for="school_email">School e-mailadres</label>
+			<input type="email" name="school_email" id="school_email" required />
 		</p>
 		<p>
-			<label for="school-address">Schooladres</label>
-			<input type="text" name="school-address" id="school-address" required />
+			<label for="school_address">Schooladres</label>
+			<input type="text" name="school_address" id="school_address" required />
 		</p>
 		<p>
-			<label for="school-website">Schoolwebsite</label>
-			<input type="url" name="school-website" id="school-website" required />
+			<label for="school_website">Schoolwebsite</label>
+			<input type="url" name="school_website" id="school_website" required />
 		</p>
 		<p>
 			<input type="submit" name="submit" value="Bestellen" />
 		</p>
 	</form>
+	<script>
+	window.app = window.app || {};
+	window.app.basename = '<?php echo $basePath;?>';
+	</script>
 	<script src="<?php echo $basePath; ?>/js/script.js"></script>
 </body>
 </html>
