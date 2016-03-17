48568999a454f4bdc423bb468b5bea095638c41f
Jasper Van Damme <jasper.van.damme@student.howest.be>
2016-03-17 13:46:24 +0100

    Removed logs from tick function

--- _js/classes/Countdown.js
+++ _js/classes/Countdown.js
@@ -2,7 +2,17 @@
 
 export default class Countdown {
 	constructor(endDate) {
+		this.startDate = new Date();
 		this.endDate = endDate;
+	}
+
+	start() {
+		this.started = true;
+		setInterval(this.tick, 1000);
+	}
+
+	tick() {
+
 	}
 
 	when() {
