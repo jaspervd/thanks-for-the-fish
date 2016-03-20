5e1741c1430eb1e04b0adf204fd6e2d252df2021
Jasper Van Damme <jasper.van.damme@student.howest.be>
2016-03-20 13:51:14 +0100

    Changed structure to other DAO's + added validation

--- dao/ClassesDAO.php
+++ dao/ClassesDAO.php
@@ -4,43 +4,72 @@ class ClassesDAO extends DAO {
 	public function getClasses() {
 		$sql = "SELECT * FROM `bw_classes` ORDER BY `id` ASC";
     $stmt = $this->pdo->prepare($sql);
-    $stmt->execute();
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    return $result;
+    if($stmt->execute()) {
+      return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    return array();
   }
 
   public function getClassById($id) {
     $sql = "SELECT * FROM `bw_classes` WHERE `id` = :id";
     $stmt = $this->pdo->prepare($sql);
     $stmt->bindValue(':id', $id);
-    $stmt->execute();
-    $result = $stmt->fetch(pdo::FETCH_ASSOC);
-    return $result;
+    if($stmt->execute()) {
+      return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    return array();
   }
 
   public function getClassesByTeacherId($id) {
     $sql = "SELECT * FROM `bw_classes` WHERE `creator_id` = :id";
     $stmt = $this->pdo->prepare($sql);
-    $stmt = $this->pdo->prepare($sql);
-    $stmt->execute();
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    return $result;
+    if($stmt->execute()) {
+      return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    return array();
   }
 
   public function insertClass($data) {
-    $sql = "INSERT INTO `bw_classes` (creator_id, nickname, num_students, photo, entry) VALUES (:creator_id, :nickname, :num_students, :photo, :entry)";
-    $stmt = $this->pdo->prepare($sql);
-    $stmt->bindValue(':creator_id', $data['creator_id']);
-    $stmt->bindValue(':nickname', $data['nickname']);
-    $stmt->bindValue(':num_students', $data['num_students']);
-    $photo = $this->uploadPhoto($data['photo']);
-    $stmt->bindValue(':photo', $photo);
-    $stmt->bindValue(':entry', $data['entry']);
-    if($stmt->execute()){
-      $insertedId = $this->pdo->lastInsertId();
-      return $this->getClassById($insertedId);
+    $errors = $this->getValidationErrors($data);
+    if(empty($errors)) {
+      $sql = "INSERT INTO `bw_classes` (creator_id, nickname, num_students, photo, entry) VALUES (:creator_id, :nickname, :num_students, :photo, :entry)";
+      $stmt = $this->pdo->prepare($sql);
+      $stmt->bindValue(':creator_id', $data['creator_id']);
+      $stmt->bindValue(':nickname', $data['nickname']);
+      $stmt->bindValue(':num_students', $data['num_students']);
+      $photo = $this->uploadPhoto($data['photo']);
+      $stmt->bindValue(':photo', $photo);
+      $stmt->bindValue(':entry', $data['entry']);
+      if($stmt->execute()){
+        $insertedId = $this->pdo->lastInsertId();
+        return $this->getClassById($insertedId);
+      }
+      return $result;
     }
-    return $result;
+    return false;
+  }
+
+  public function getValidationErrors($data) {
+    $errors = array();
+
+    if(empty($data['nickname'])) {
+      $errors['score'] = "Please enter a nickname.";
+    }
+
+    if(empty($data['num_students']) || is_nan($data['num_students']) || $data['num_students'] < 0) {
+      $errors['score'] = "Please enter a valid number of students.";
+    }
+
+    $imageMimeTypes = array('image/jpeg', 'image/png', 'image/gif');
+    if (empty($data['photo']) || in_array($data['photo']['type'], $imageMimeTypes)) {
+      $errors['photo'] = "Please upload a valid photo.";
+    }
+
+    if(empty($data['entry'])) {
+      $errors['entry'] = "Please type your book review.";
+    }
+
+    return $errors;
   }
 
   public function deleteClass($id) {
@@ -51,21 +80,17 @@ class ClassesDAO extends DAO {
   }
 
   public function uploadPhoto($photo) {
-    $imageMimeTypes = array('image/jpeg', 'image/png', 'image/gif');
-    if (in_array($photo['type'], $imageMimeTypes)) {
-      $targetFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'upload' . DIRECTORY_SEPARATOR . $photo['name'];
-      $pos = strrpos($targetFile, '.');
-      $filename = substr($targetFile, 0, $pos);
-      $ext = substr($targetFile, $pos + 1);
-      $i = 0;
-      while (file_exists($targetFile)) {
-        $i++;
-        $targetFile = $filename . $i . '.' . $ext;
-      }
-      move_uploaded_file($photo['tmp_name'], $targetFile);
-      $photo_url = str_replace(dirname(__DIR__) . DIRECTORY_SEPARATOR, '', $targetFile);
-      return $photo_url;
+    $targetFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'upload' . DIRECTORY_SEPARATOR . $photo['name'];
+    $pos = strrpos($targetFile, '.');
+    $filename = substr($targetFile, 0, $pos);
+    $ext = substr($targetFile, $pos + 1);
+    $i = 0;
+    while (file_exists($targetFile)) {
+      $i++;
+      $targetFile = $filename . $i . '.' . $ext;
     }
-    return false;
+    move_uploaded_file($photo['tmp_name'], $targetFile);
+    $photo_url = str_replace(dirname(__DIR__) . DIRECTORY_SEPARATOR, '', $targetFile);
+    return $photo_url;
   }
 }
