82a70a15defe74d874e474fad2bc18fc7111e168
Jasper Van Damme <jasper.van.damme@student.howest.be>
2016-03-24 01:24:29 +0100

    Added style for login

--- view/login.php
+++ view/login.php
@@ -1,37 +1,58 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<base href="<?php echo $basePath; ?>/">
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<title>Login | Klassiekers in je klas</title>
-	<link rel="stylesheet" href="<?php echo $basePath; ?>/css/style.css">
+	<link rel="stylesheet" href="css/style.css">
 </head>
-<body>
-	<div class="container">
-		<section>
-			<header>
-				<h1>Login</h1>
+<body class="login">
+	<div class="wrapper">
+		<div class="logo">
+			<a href="http://boek.be">
+				<img src="images/logo.png" srcset="images/logo.png 1x, images/logo@2x.png 2x" alt="Logo boek.be" />
+			</a>
+		</div>
+		<nav class="menu closed">
+			<header class="hide">
+				<h1>Menu</h1>
 			</header>
-			<form method="post" action="" class="login-form">
-				<p>
-					<label for="login-email">E-mailadres</label>
-					<input type="email" name="email" id="login-email" required />
-				</p>
-				<p>
-					<label for="login-password">Wachtwoord</label>
-					<input type="password" name="password" id="login-password" required />
-				</p>
-				<p>
-					<input type="submit" name="submit" value="Bestellen" />
-				</p>
-			</form>
-		</section>
+			<a href="#" class="menu-toggle button">&#9776;</a>
+			<ul>
+				<li><a href="#page-0" class="nav-menu button active">Campagne</a></li>
+				<li><a href="#page-1" class="nav-menu button">Boek</a></li>
+				<li><a href="#page-2" class="nav-menu button">Voorproefje</a></li>
+				<li><a href="#page-3" class="nav-menu button">Klasfoto's</a></li>
+				<li><a href="login" class="button">Login</a></li>
+			</ul>
+		</nav>
+		<main class="container">
+			<section class="page-screen">
+				<header class="hide">
+					<h1>Login</h1>
+				</header>
+				<form method="post" action="" class="login-form">
+					<p class="input-wrapper">
+						<label for="login-email">E-mailadres</label>
+						<input type="email" name="email" id="login-email" required />
+					</p>
+					<p class="input-wrapper">
+						<label for="login-password">Wachtwoord</label>
+						<input type="password" name="password" id="login-password" required />
+					</p>
+					<p class="input-wrapper center">
+						<input type="submit" name="submit" value="Inloggen" class="button submit" />
+					</p>
+				</form>
+			</section>
+		</main>
 	</div>
 	<script>
 	window.app = window.app || {};
 	window.app.basename = '<?php echo $basePath;?>';
 	</script>
-	<script src="<?php echo $basePath; ?>/js/login.js"></script>
+	<script src="js/login.js"></script>
 </body>
 </html>
